60fb5f81a1a5f039b2952c0515b3e71a6912cebd
Roland <dfidentity@gmail.com>
2021-04-03 11:17:54 +0300

    Tidy up the code.
    I've changed the structure of the code to be shorter and more readable.
    I can also now add different types of images (cats, etc.) easily (sorta).

--- main.go
+++ main.go
@@ -13,58 +13,44 @@ import (
 	"time"
 )
 
-type DogJSON struct {
-	Message string
-}
-
+type DogJSON struct { Message string }
 var totalDownloaded int
 
-func downloadFile(url string, filepath string, finished chan float64){
-	resp, err := http.Get(url)
+func downloadFile(url string, filepath string) float64{
+	resp, err := http.Get(url); defer resp.Body.Close()
 	if err != nil {
 		fmt.Println(err)
-		finished <- 1
-		return
+		return 1
 	}
-	defer resp.Body.Close()
-	out, err := os.Create(filepath)
+	out, err := os.Create(filepath); defer out.Close()
 	if err != nil {
 		fmt.Println(err)
-		finished <- 1
-		return
+		return 1
 	}
-	defer out.Close()
 	_, err = io.Copy(out, resp.Body)
-	finished <- 1
 	totalDownloaded += 1
 	fmt.Println("Downloaded "+filepath)
-	return
+	return 1
 }
 
-func getDogs(finished chan float64, iter int) {
-	//resp, err := http.Get("https://api.thedogapi.com/v1/images/search")
+func processDogs(finished chan float64, iter int){ finished <- getDogs(finished, iter) }
+func processDownload(url string, filepath string, finished chan float64){ finished <- downloadFile(url, filepath) }
+
+func getDogs(finished chan float64, iter int) float64 {
 	resp, err := http.Get("https://dog.ceo/api/breeds/image/random")
 	if err != nil{
-		fmt.Println(err)
-		finished <- 2
-		return
+		return 2
 	}
 	bytes, _ := ioutil.ReadAll(resp.Body)
 
 	var decoded DogJSON
 	json.Unmarshal(bytes, &decoded)
-	/*if len(decoded) == 0 {
-		fmt.Println("API Error [IGNORING]")
-		finished <- 2
-		return
-	}*/
 
 	var url = decoded.Message
 	fileName := strings.Split(url, "/")[len(strings.Split(url, "/"))-1]
 	fmt.Println("Downloading "+fileName+" (Thread "+strconv.Itoa(iter)+")")
-	finished <- 1
-	go downloadFile(url, "Dogs/"+fileName, finished)
-	return
+	go processDownload(url, "Dogs/"+fileName, finished)
+	return 1
 }
 
 func loopUntilDone(finished chan float64, fullFin chan int, iters float64) {
@@ -76,26 +62,24 @@ func loopUntilDone(finished chan float64, fullFin chan int, iters float64) {
 }
 
 func main() {
-	fmt.Println("====\nDog Downloader\n====\nPress CTRL+C to interrupt.\nDogs will be saved in [RUNPATH]/Dogs.\n")
-	fmt.Print("Enter how many dogs you want: ")
+	fmt.Print("====\nDog Downloader\n====\nPress CTRL+C to interrupt.\nDogs will be saved in [RUNPATH]/Dogs.\n\nEnter how many dogs you want: ")
 	var iters float64
 	fmt.Scanln(&iters)
 
-	finished := make(chan float64)
-	fullFin := make(chan int)
+	finished, fullFin := make(chan float64), make(chan int)
 
 	os.Mkdir("Dogs", 0755)
-
 	go loopUntilDone(finished, fullFin, iters)
 
 	var smCheck float64
 	if iters<200 { smCheck = iters } else { smCheck = math.Ceil(iters/math.Ceil(iters/200)) }
+
 	fmt.Println("Starting threads...")
 	for j := 0.0; j<math.Ceil(iters/200); j++{
 		for i := 0.0; i < smCheck; i++ {
-			go getDogs(finished, int(i))
+			go processDogs(finished, int(i))
 		}
-		time.Sleep(1*time.Second)
+		time.Sleep(time.Second)
 	}
 
 	fmt.Println("All threads online!")
